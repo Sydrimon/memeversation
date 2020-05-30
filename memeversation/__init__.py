d55653492c9346a0a1f2e6274b9480977cdcfeae
Sydrimon <sydrimon@gmail.com>
2020-05-30 15:52:27 +0200

    change project structure

---
+++ memeversation/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from pathlib import Path
+
+class Memeversation:
+
+    def __init__(self, data_file=Path('data/gifs.json')):
+        self.data_file = data_file
+        self.last_modified = 0
+        self.gif_data = {}
+
+        self.add_gif_data(self.data_file)
+
+    def add_gif_data(self, gif_json):
+        self.gif_data.update(json.loads(gif_json.open().read()))
+
+    def empty_gif_data(self):
+        self.gif_data.clear()
+        
+    def update_gif_data(self):
+        if self.last_modified < self.data_file.stat().st_mtime:
+            self.add_gif_data(self.data_file)
+            self.last_modified = self.data_file.stat().st_mtime
+
+    def find_word(self, text):
+        for w in text.lower().split():
+            if w in self.gif_data:
+                return self.gif_data[w]
+
+    def write_data_file(self):
+        json.dump(self.gif_data, self.data_file.open('w'))
--- telegram_bot.py
+++ telegram_bot.py
@@ -2,32 +2,18 @@
 # -*- coding: utf-8 -*-
 
 """
-Bot to display food of the TH Rosenheim canteen
+Bot to your conversation with beautiful gifs
 """
 
-import json
+from memeversation import Memeversation
 import logging
-import os
-from pathlib import Path
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, JobQueue
 
-data_file = Path('gifs.json')
-last_modified = 0
-gif_data = {}
+m = Memeversation()
 
 # logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-def populate_gif_data():
-    gif_data.clear()
-    gif_data.update(json.loads(data_file.open().read()))
-    last_modified = data_file.stat().st_mtime
-
-def find_word(text):
-    for w in text.lower().split():
-        if w in gif_data:
-            return gif_data[w]
 
 def start(update, context):
     """Send a message when the command /start is issued."""
@@ -38,9 +24,8 @@ def help(update, context):
     update.message.reply_text('Bischd du dumm?')
 
 def read(update, context):
-    if last_modified < data_file.stat().st_mtime:
-        populate_gif_data()
-    match = find_word(update.message.text)
+    m.update_gif_data()
+    match = m.find_word(update.message.text)
     if match:
         update.message.reply_animation(match)
 
@@ -50,7 +35,6 @@ def error(update, context):
 
 def main():
     # init
-    populate_gif_data()
     updater = Updater("", use_context=True)
     dp = updater.dispatcher
 
