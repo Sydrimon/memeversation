c5eb794d0cb940739ea2db86dca39900ad5d6cc9
Sydrimon <sydrimon@gmail.com>
2024-02-24 22:37:22 +0100

    update python-telegram-bot to v20.8
    fix #15

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,7 +8,8 @@ Bot to enrich your conversation with beautiful gifs
 import logging
 from memeversation import Memeversation
 from memeversation import Tracking
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, JobQueue
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 m = Memeversation()
 stats = Tracking()
@@ -16,48 +17,46 @@ stats = Tracking()
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def start(update, context):
+async def start(update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    await update.message.reply_text('Hi!')
 
-def help(update, context):
+async def help(update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Bischd du dumm?')
+    await update.message.reply_text('Bischd du dumm?')
 
-def usage(update, context):
+async def usage(update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """send usage of requested phrase"""
     phrase = " ".join(context.args).lower()
     used = "{0} was used {1} times".format(phrase, stats.evaluate_word(phrase))
-    update.message.reply_text(used)
+    await update.message.reply_text(used)
 
-def read(update, context):
+async def read(update, context: ContextTypes.DEFAULT_TYPE) -> None:
     m.update_gif_data()
     word, media = m.get_gif(update.message.text.lower())
     if media:
-        update.message.reply_animation(media)
-
         # collect stats
         stats.update(update.message.from_user.username, word,
             update.message.chat.title)
 
-def error(update, context):
+        await update.message.reply_animation(media)
+
+def error(update, context: ContextTypes.DEFAULT_TYPE):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
-    updater = Updater("", use_context=True)
-    dp = updater.dispatcher
+    application = Application.builder().token("").build()
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("hilfe", help))
-    dp.add_handler(CommandHandler("nutzung", usage))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("hilfe", help))
+    application.add_handler(CommandHandler("nutzung", usage))
 
-    dp.add_handler(MessageHandler(Filters.text, read))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, read))
 
-    dp.add_error_handler(error)
+    application.add_error_handler(error)
 
-    updater.start_polling()
-    updater.idle()
+    application.run_polling(allowed_updates=Update.MESSAGE)
 
 if __name__ == '__main__':
     main()
